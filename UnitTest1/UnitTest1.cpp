fd88a5a03a62555b553c9c9c24475b19f5279a62
misha-fedyna <mihajlofedina5@gmail.com>
2024-11-11 14:43:45 +0200

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab11.1C.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        // Тест для countCharacters
+        TEST_METHOD(TestCountCharacters)
+        {
+            const char* testString = "++--==";
+            int plusCount = 0, minusCount = 0, equalCount = 0;
+
+            countCharacters(testString, &plusCount, &minusCount, &equalCount);
+
+            Assert::AreEqual(2, plusCount);    // Очікується 2 символи '+'
+            Assert::AreEqual(2, minusCount);   // Очікується 2 символи '-'
+            Assert::AreEqual(2, equalCount);   // Очікується 2 символи '='
+        }
+    };
+}
---
+++ lab11.1C.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+// Функція для підрахунку входжень заданих символів у рядку
+void countCharacters(const char* line, int* plusCount, int* minusCount, int* equalCount) {
+    for (int i = 0; line[i] != '\0'; i++) {
+        if (line[i] == '+') {
+            (*plusCount)++;
+        }
+        else if (line[i] == '-') {
+            (*minusCount)++;
+        }
+        else if (line[i] == '=') {
+            (*equalCount)++;
+        }
+    }
+}
+
+// Функція для зчитування вмісту файлу і підрахунку символів
+void countSymbolsInFile(const char* filename, int* plusCount, int* minusCount, int* equalCount) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Не вдалося відкрити файл: %s\n", filename);
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        countCharacters(line, plusCount, minusCount, equalCount);
+    }
+
+    fclose(file);
+}
+
+// Основна функція
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const char* filename = "t.txt";
+    int plusCount = 0, minusCount = 0, equalCount = 0;
+
+    countSymbolsInFile(filename, &plusCount, &minusCount, &equalCount);
+
+    printf("Кількість входжень символів:\n");
+    printf("'+': %d\n", plusCount);
+    printf("'-': %d\n", minusCount);
+    printf("'=': %d\n", equalCount);
+
+    return 0;
+}
